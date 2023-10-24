32805acee1aa1a6213fcbbc225e6f841869adf28
caoguosheng <caoguosheng.smg.cn>
2023-10-24 18:01:18 +0800

    no message

--- flutter_web_study1/lib/Models/Network/net_api.dart
+++ flutter_web_study1/lib/Models/Network/net_api.dart
@@ -1,4 +1,17 @@
 
 class NetApi{
+  Map getRequestMap(Map aMap){
+
+
+    aMap["version"] = "4.0.6";
+    aMap["platform"] = "ios";
+
+    return aMap;
+  }
+
+  String getToken(){
+    var token = "";
+    return token;
+  }
 
 }
