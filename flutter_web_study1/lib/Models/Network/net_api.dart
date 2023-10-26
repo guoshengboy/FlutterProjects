c3ad9724016336889abbb40951fd940107e456fc
caoguosheng <caoguosheng.smg.cn>
2023-10-26 09:36:33 +0800

    no message

--- flutter_web_study1/lib/Models/Network/net_api.dart
+++ flutter_web_study1/lib/Models/Network/net_api.dart
@@ -1,15 +1,31 @@
 
+import 'dart:math';
+
 class NetApi{
   Map getRequestMap(Map aMap){
 
-
+    var rand = Random().nextInt(100000);
+    DateTime now = DateTime.now();
+    int timestamp = now.millisecondsSinceEpoch;
+    aMap["ts"] = timestamp;
+    aMap["rand"] = rand;
     aMap["version"] = "4.0.6";
     aMap["platform"] = "ios";
 
     return aMap;
   }
 
-  String getToken(){
+  String getToken(Map paramsMap, String rand){
+    DateTime now = DateTime.now();
+    int timestamp = now.millisecondsSinceEpoch;
+    var firstString = rand + timestamp.toString();
+    //循环获取keyList
+    List<String> keyList = [];
+    paramsMap.forEach((key, value) {
+      keyList.add(key);
+    });
+    //数组key排序
+    keyList.sort();
     var token = "";
     return token;
   }
